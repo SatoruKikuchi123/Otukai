d67eb022cca3cc2d662b938aae6a49c7ed4d3d9d
togami428 <98520065+togami428@users.noreply.github.com>
2022-03-07 14:52:55 +0900

    Update myCody.js

--- myCody.js
+++ myCody.js
@@ -6,16 +6,16 @@ const goods = document.querySelector('#goods');
 const number = document.querySelector('#numberGoods');
 //登録ボタンの要素をsubmitに代入
 const submit = document.getElementsByClassName('submit')[0];
-//ulタグの要素をrequesterListに入れる
+//tableタグの要素をtableに入れる
 const table = document.getElementsByClassName('table')[0];
 
 
 //登録ボタンをクリックし、イベントを発動（依頼者を追加）
-submit.addEventListener('click', evt => {
+submit.addEventListener('click', () => {
 
     // 選択した依頼者を追加・表示
     //セレクタの中身が入っているか確認　
-    if (requester.value !== "") {
+    if (requester.value !== "" && goods.value !== "" && number.value !== "") {
         let table = document.getElementById('targetTable');
         let newRow = table.insertRow();
 
@@ -31,27 +31,26 @@ submit.addEventListener('click', evt => {
         newText = document.createTextNode(number.value);
         newCell.appendChild(newText);
 
-
-
-        //依頼者に削除ボタンを付与
-        //button要素を生成
-        const deleteButton = document.createElement('button');
-        // 生成したボタンの中に削除を書き込み
+        newCell = newRow.insertCell();
+        let deleteButton = document.createElement('button');
         deleteButton.innerHTML = '削除';
-        //liタグの子に削除ボタンを追加
-        table.appendChild(deleteButton);
+        newCell.appendChild(deleteButton);
 
         //削除ボタンをクリックし、イベントを発動（タスクが削除）
-        deleteButton.addEventListener('click', evt => {
-            //     deleteTasks(deleteButton);
+        deleteButton.addEventListener('click', () => {
+            deleteTasks(deleteButton);
         });
+    } else {
+        alert("項目を全て選択してください！！");
     }
+
 });
 
 
 //削除ボタンにタスクを消す機能を付与
 const deleteTasks = (deleteButton) => {
-    //closest : deleteButtonの親要素の中からliを含む最も近い要素を返す
-    const chosenTask = deleteButton.closest('targetTable');
-    table.removeChild(chosenTask);
+    // 削除ボタンを押下された行を取得
+    const pushTr = deleteButton.parentNode.parentNode;
+    // pushTrのインデックスを取得して行を削除する
+    pushTr.parentNode.deleteRow(pushTr.sectionRowIndex);
 };
